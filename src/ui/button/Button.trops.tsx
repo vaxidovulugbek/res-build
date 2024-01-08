a8b3774fcf314c17dd69b23255a8de71c0f8d9b5
vakhidovulugbek03 <vulugbekk@gmail.com>
2024-01-08 21:23:02 +0500

    add ui components

---
+++ src/ui/button/Button.trops.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export type ButtonProps = {
+	variant?: "primary" | "filled" | "outlined" | "danger";
+	size?: "lg" | "md" | "sm" | "icon";
+	text?: string;
+	isDisabled?: boolean;
+	isLoading?: boolean;
+	roundness?: "half" | "full" | false;
+	link?: string;
+} & Omit<React.ComponentPropsWithoutRef<"button">, "disabled">;
