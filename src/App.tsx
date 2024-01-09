aeae706dce568e3a6ca928b9bc5aa46b854a6969
vakhidovulugbek03 <vulugbekk@gmail.com>
2024-01-09 11:49:48 +0500

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
 	return (
 		<div className="App">
-			<h1>hello world nn tt</h1>
+			<h1 className="text-6xl text-tahiti bg-midnight font-bold">hello world tt</h1>
 		</div>
 	);
 }
