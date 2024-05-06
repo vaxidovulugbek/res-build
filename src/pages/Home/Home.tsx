c0e598f3507c88436c14db9ec0d17e1c98dc8ed6
vakhidovulugbek03 <vulugbekk@gmail.com>
2024-05-06 18:58:28 +0500

    fix

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,8 +7,8 @@ const Home: React.FC = () => {
 			<Header />
 			<main>
 				<Hero />
-				<PopularProducts />
 				<HowItWorks />
+				<PopularProducts />
 				<Making_Resume />
 				<Faq />
 			</main>
